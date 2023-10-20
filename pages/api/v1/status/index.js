f4fd5e5db5de862b4375449182d512a35d06b524
Guedes Codes <guedescode@gmail.com>
2023-10-20 09:42:54 -0300

    Inicio configuracao do modulo

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(request, response) {
-    response.status(200).json({"chave":"valorrr"})
+import database from "../../../../infra/database.js";
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows[0]);
+  response.status(200).json({ chave: "valorrr" });
 }
