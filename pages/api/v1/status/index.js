a5ab916768a0ee3fdc884bd9eeb06a2d4bcab3cc
Guedes Codes <guedescode@gmail.com>
2024-03-05 20:54:24 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,36 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows[0]);
-  response.status(200).json({ chave: "valorrr" });
+  const updateAt = new Date().toISOString();
+
+  //versao do postgress
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersion = dbVersionResult.rows[0].server_version;
+
+  //quantidade de conexoes maximas utilizadas,
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnections = dbMaxConnectionsResult.rows[0].max_connections;
+
+  //quantas conexoes estao sendo usadas
+  const databaseName = process.env.POSTGRES_DB;
+  //console.log(`Banco de dados selecionado ${databaseName}`);
+  const dbOpenedConnectionsResult = await database.query({
+    text: `SELECT count(*):: int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  //("SELECT count(*):: int FROM pg_stat_activity WHERE datname = 'local_db'");
+  const dbOpenedConnections = dbOpenedConnectionsResult.rows[0].count;
+
+  const statusCompleted = {
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(dbMaxConnections),
+        opened_connections: dbOpenedConnections,
+      },
+    },
+  };
+
+  response.status(200).json(statusCompleted);
 }
